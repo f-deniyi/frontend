4f42902d267c74254a9371283f9f5c8b7733f735
f-deniyi <holarfemilekan049@gmail.com>
2020-06-08 21:07:13 +0100

    feat: Script for the testimonial section

--- Group4/assets/js/main.js
+++ Group4/assets/js/main.js
@@ -1,1 +1,8 @@
+let slides=document.querySelectorAll('.slide');
+counter=0;
 
+const showSlide=(n)=>{
+  slides[n].classList.remove('active-slide');
+  slides[counter].classList.add('active-slide');
+}
+showSlide(counter);
